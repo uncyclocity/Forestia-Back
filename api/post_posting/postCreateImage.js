d0615742b272f384d2ba5df0c8f64794a95d1e0b
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-10-15 17:12:46 +0900

    [UPDATE] aws s3 연동 셋팅

--- api/post_posting/postCreateImage.js
+++ api/post_posting/postCreateImage.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const s3 = new aws.S3({
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  region: process.env.region,
+});
 const fs = require("fs");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, `public/uploads/${req.query.board_type}`);
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`);
+const storage = multerS3({
+  s3: s3,
+  bucket: "itsforestia",
+  acl: "public-read",
+  key: function (req, file, cb) {
+    cb(
+      null,
+      Math.floor(Math.random() * 1000).toString() +
+        Date.now() +
+        "." +
+        file.originalname.split(".").pop()
+    );
   },
 });
 
